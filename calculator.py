eec06c8b132bf82a95a37d25a29a2fb9b8caa50e
Landon <landonmarshall1997@gmail.com>
2019-12-21 12:35:33 -0800

    Hands are now created into a list

--- calculator.py
+++ calculator.py
@@ -1,15 +1,47 @@
 import sys
 from card import Card
 
+suits = ['d', 'h', 's', 'c']
+values = ['2','3','4','5','6','7','8','9','10','J','H','K','A']
+hands_list = []
+building_hand = []
 
-while(1):
-    new_card = input("Enter a card (Format Like 2s/ Jd/ Ah), or exit to terminate: ")
-    if new_card in ['Exit', 'exit']:
-        sys.exit()
+# Validate input string to determine if we can create new card object
+def valid_card(value, suit):
+    if value in values and suit in suits:
+        return True
     else:
-        value = new_card[0:len(new_card)-1]
-        suit = new_card[-1]
-        card = Card(value, suit)
-        print(card)
+        return False
 
-    
+# Print out the current hand being built, as well as the list of hands
+def print_hands():
+    print ('Current hand: ', building_hand)
+    print ('Hands after new addition: ', hands_list)
+
+# Build a hand and if the hand is complete, add it to the list of hands
+def build_hand(card):
+    global building_hand
+    if len(building_hand) == 0 :
+        building_hand.append(card)
+    else: 
+        building_hand.append(card)
+        hands_list.append(building_hand)
+        building_hand = []
+
+def main():
+    while(1):
+        new_card = input("Enter a card (Format Like 2s/ Jd/ Ah), or exit to terminate: ")
+        if new_card in ['Exit', 'exit']:
+            sys.exit()
+        else:
+            value = new_card[0:len(new_card)-1]
+            suit = new_card[-1]
+            if valid_card(value, suit):
+                card = Card(value, suit)
+                build_hand(card)
+            else:
+                print('Invalid card format')
+            print_hands()
+
+if __name__== "__main__":
+    main()
--- card.py
+++ card.py
@@ -1,15 +1,14 @@
-suits = {'d': 'Diamonds', 'h': 'Hearts', 's': 'Spades', 'c': 'Clubs'}
-values = {2: 2, 3: 3, 4: 4, 5:5, 6:6, 7:7, 8:8, 9:9, 10: 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14}
-print_values = {2: 2, 3: 3, 4: 4, 5:5, 6:6, 7:7, 8:8, 9:9, 10: 10, 'J': 'Jack', 'Q': 'Queen', 'K': 'King', 'A': 'Ace'}
+print_values = {'2': '2', '3': '3', '4': '4', '5': '5', '6': '6', '7': '7', '8': '8', '9': '9', '10': '10', 
+'J': 'Jack', 'Q': 'Queen', 'K': 'King', 'A': 'Ace'}
+print_suits = {'d': 'Diamonds', 'h': 'Hearts', 's': 'Spades', 'c': 'Clubs'}
 
 class Card:
     def __init__(self, value, suit):
-        validate_input(self, value, suit)
         self.value = value
         self.suit = suit
 
     def __str__(self):
-        return print_values[self.value] + " of " + suits[self.suit]
+        return print_values[self.value] + " of " + print_suits[self.suit]
 
-    def validate_input(value, suit):
-        
+    def __repr__(self):
+        return self.value + self.suit 
