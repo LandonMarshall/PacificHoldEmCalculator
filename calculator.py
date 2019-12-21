53c8ceda33c775c03d1a3e1cb8bbbd5bf94e53f1
Landon <landonmarshall1997@gmail.com>
2019-12-21 15:51:26 -0800

    GUI is functional for adding cards

--- calculator.py
+++ calculator.py
@@ -1,53 +1,46 @@
 import sys
-from tkinter import Tk, Button, Grid, Frame, N, E, S, W, PhotoImage
+import os
+from tkinter import Tk, Button, Grid, Frame, N, E, S, W, PhotoImage, Image
 from card import Card
 
 
 suits = ['d', 'h', 's', 'c']
-values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 hands_list = []
 building_hand = []
-
 
 class OddsCalculator():
     def __init__(self, master):
         self.master = master
-
+        self.images = {}
         master.geometry("1000x400")
-        master.resizable(width=False, height=False)
+        master.resizable(width=True, height=False)
         master.title("Odds Calculator")
-
         frame = Frame(master)
         frame.grid(row=0, column=0, sticky=N+S+E+W)
-
         i = 0
 
         Grid.rowconfigure(master, 0, weight=1)
         Grid.columnconfigure(master, 0, weight=1)
 
+        # Create a button for each card on UI
         for row_index in range(len(suits)):
             suit = suits[row_index]
             Grid.rowconfigure(frame, row_index, weight=1)
-
             for col_index in range(len(values)):
                 value = values[col_index]
                 Grid.columnconfigure(frame, col_index, weight=1)
-                self.button = Button(frame, text = value + " " + suit, command= lambda value = value, suit = suit : self.create_card(value, suit))
-                # homescreenImage = PhotoImage(file="/Users/landonmarshall/Desktop/PacificHoldEmCalculator/Cards/" + value+suit.capitalize()+".jpg") 
-                self.button.config(image = "/Users/landonmarshall/Desktop/PacificHoldEmCalculator/Cards/"+ value+suit.capitalize()+".jpg")
-                self.button.grid(row=row_index, column=col_index, sticky=N+S+E+W)  
-        # for value in values:
-        #     self.button = Button(master, text = value + "S", command= lambda value = value, suit = 's' : self.create_card(value, suit))
-        #     self.button.grid(row = 0, column = i, columnspan=13, sticky=N+S+E+W)
-        #     i = i + 1
-        # for value in values:
-        #     self.button = Button(master, text = value + "D", command= lambda value = value, suit = 's' : self.create_card(value, suit))
-        #     self.button.grid(row = 1, column = i, columnspan=13, sticky=N+S+E+W)
-        #     i = i + 1
-        # self.twos = Button(master, text="Two S", command= lambda value = '2', suit = 's' : self.create_card(value, suit))
-        # self.twos.grid(row=0, column=0, columnspan=13, sticky="we")
-        # self.twod = Button(master, text="Two D", command= lambda value = '2', suit = 'd' : self.create_card(value, suit))
-        # self.twod.grid(row=1, column=0, columnspan=13, sticky="we")
+                self.images[value+ " " + suit] = PhotoImage(file= "2D.ppm")
+                self.button = Button(frame,
+                    text = value + "" + suit, 
+                    width = 25,
+                    height = 35,
+                    fg =  '#ff0000' if row_index < 2 else '#000000',
+                    # image = self.images[value+" " + suit],
+                    command= lambda value = value, 
+                    suit = suit : self.create_card(value, suit))
+                self.button.grid(row=row_index, column=col_index)  
+                i = i + 1
 
     def valid_card(self, value, suit):
         """ Validate input string to determine if input is valid card format """
@@ -77,26 +70,10 @@ class OddsCalculator():
             hands_list.append(building_hand)
             building_hand = []
 
-
 def main():
     root = Tk()
     gui = OddsCalculator(root)
     root.mainloop()
-
-    # while(1):
-    #     new_card = input("Enter a card (Format Like 2s/ Jd/ Ah), or exit to terminate: ")
-    #     if new_card in ['Exit', 'exit']:
-    #         sys.exit()
-    #     else:
-    #         value = new_card[0:len(new_card)-1]
-    #         suit = new_card[-1]
-    #         if valid_card(value, suit):
-    #             card = Card(value, suit)
-    #             build_hand(card)
-    #         else:
-    #             print('Invalid card format')
-    #         print_hands()
-
 
 if __name__ == "__main__":
     main()
